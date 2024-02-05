76502a7ec1a49eaa526d65d2fcfaf9e900f8987f
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-02-05 16:16:20 +0000

    updated env

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
 
     useEffect(() => {
         const getDribbleShots = async () => {
-            const dribbleAccessToken = `${process.env.REACT_APP_DRIBBBLE_ACCESS_TOKEN}`;
+            const dribbleAccessToken = `${process.env.REACT_APP_DRIBBLE_ACCESS_TOKEN}`;
             const dribbblePage = 1;
             const SHOTS_PER_PAGE = 10;
             const dribbleapi = `https://api.dribbble.com/v2/user/shots?access_token=${dribbleAccessToken}&page=${dribbblePage}&per_page=${SHOTS_PER_PAGE}`;
@@ -26,6 +26,7 @@ const Projects = () => {
             try {
                 const dResponseJson = await dResponse.json();
                 const dshots = dResponseJson;
+                console.log(dResponseJson);
                 setShots(dshots);
                 console.log(dshots);
             } catch (error) {
@@ -94,22 +95,38 @@ const Projects = () => {
                 </Box>
             </Box>
             <div className="arrowiconBtn2">
-                <ArrowUpward style={{ display: visible ? "inline-block" : "none" }} className="arrowicon2" onClick={scrollToTop} />
+                <ArrowUpward
+                    style={{ display: visible ? "inline-block" : "none" }}
+                    className="arrowicon2"
+                    onClick={scrollToTop}
+                />
             </div>
             <Box className="main-project-section">
                 {shots
                     ? shots?.map((shot) => {
                           return (
-                              <Box className="project-section" data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine" key={shots.id}>
+                              <Box
+                                  className="project-section"
+                                  data-aos="fade-right"
+                                  data-aos-offset="300"
+                                  data-aos-easing="ease-in-sine"
+                                  key={shots.id}>
                                   <Box className="main-card">
                                       <Box className="card-img">
                                           <img src={shot.images.normal} alt="cardimg" className="card-img-inner" />{" "}
                                       </Box>
                                       <Box className="card-title"> {shot.title} </Box>
-                                      <Box className="card-subtitle"> {Date(shot.updated_at).toString().slice(0, 15)}</Box>
+                                      <Box className="card-subtitle">
+                                          {" "}
+                                          {Date(shot.updated_at).toString().slice(0, 15)}
+                                      </Box>
                                       <Box className="card-footer">
                                           <Typography className="loadshot-text"> Load Shot </Typography>
-                                          <FontAwesomeIcon className="shots-icon" icon={faDribbble} onClick={onClickUrl(`${shot.html_url}`)} />
+                                          <FontAwesomeIcon
+                                              className="shots-icon"
+                                              icon={faDribbble}
+                                              onClick={onClickUrl(`${shot.html_url}`)}
+                                          />
                                       </Box>
                                   </Box>
                               </Box>
@@ -129,17 +146,28 @@ const Projects = () => {
                 {articles
                     ? articles.map((posts) => {
                           return (
-                              <Box className="main-blog-card" key={posts.link} data-aos="fade-up" data-aos-duration="3000">
+                              <Box
+                                  className="main-blog-card"
+                                  key={posts.link}
+                                  data-aos="fade-up"
+                                  data-aos-duration="3000">
                                   <Box className="card-blog-img">
                                       <img className="blog-img" src={posts.thumbnail} alt="blogimg" />
                                   </Box>
                                   <Box className="card-blog-content">
                                       <Box className="blog-content-main">
                                           <p className="blog-content-title">{posts.title}</p>
-                                          <Markup className="blog-content-overview" content={posts.description}></Markup>
+                                          <Markup
+                                              className="blog-content-overview"
+                                              content={posts.description}></Markup>
                                       </Box>
                                       <Box className="blog-content-fotter">
-                                          <Button variant="contained" fontSize="small" className="readmore-btn" href={posts.link} target="_blank">
+                                          <Button
+                                              variant="contained"
+                                              fontSize="small"
+                                              className="readmore-btn"
+                                              href={posts.link}
+                                              target="_blank">
                                               Read More
                                           </Button>
                                           <p className="blog-fotter-date">{posts.pubDate.slice(0, 10)}</p>
@@ -160,29 +188,47 @@ const Projects = () => {
                     </Box>
                     <Box style={{ marginBottom: "1%", marginTop: "1%" }}>
                         <Accordion>
-                            <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel1a-content" id="panel1a-header">
+                            <AccordionSummary
+                                expandIcon={<ExpandMore />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header">
                                 <h3>MovieHunt </h3>
                             </AccordionSummary>
                             <AccordionDetails>
                                 <Typography>
-                                    A movie web app where a user can search and watch for any of his favourite movie trailer or tv show trailer.{" "}
-                                    <span style={{ fontWeight: "bolder" }}>New feature comming soon : </span>Where user can watch movies, add to watchlist and share with friends and family.
-                                    <Button href="https://timepasswebapp.herokuapp.com/" target="_blank" rel="noopener noreferrer" color="inherit">
+                                    A movie web app where a user can search and watch for any of his favourite movie
+                                    trailer or tv show trailer.{" "}
+                                    <span style={{ fontWeight: "bolder" }}>New feature comming soon : </span>Where user
+                                    can watch movies, add to watchlist and share with friends and family.
+                                    <Button
+                                        href="https://timepasswebapp.herokuapp.com/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        color="inherit">
                                         Click to view
                                     </Button>
                                 </Typography>
                             </AccordionDetails>
                         </Accordion>
                         <Accordion>
-                            <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel1a-content" id="panel1a-header">
+                            <AccordionSummary
+                                expandIcon={<ExpandMore />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header">
                                 <h3>SendCryp </h3>
                             </AccordionSummary>
                             <AccordionDetails>
                                 <Typography>
-                                    A crypto web app where a user can send any cryptocurrencies around the world on ethereum blockhain.{" "}
-                                    <span style={{ fontWeight: "bolder" }}>New feature comming soon : </span> Where user can keep track of price change, add to watchlist and receive email notification
-                                    on funds received.
-                                    <Button href="https://sendcryp-afebd.web.app/home" target="_blank" rel="noopener noreferrer" color="inherit">
+                                    A crypto web app where a user can send any cryptocurrencies around the world on
+                                    ethereum blockhain.{" "}
+                                    <span style={{ fontWeight: "bolder" }}>New feature comming soon : </span> Where user
+                                    can keep track of price change, add to watchlist and receive email notification on
+                                    funds received.
+                                    <Button
+                                        href="https://sendcryp-afebd.web.app/home"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        color="inherit">
                                         Click to view
                                     </Button>
                                 </Typography>
@@ -198,7 +244,10 @@ const Projects = () => {
                     <a href="https://twitter.com/tapesh2" target="_blank" rel="noopener noreferrer">
                         <Twitter style={{ fontSize: "2vw ", color: "#FE4747" }} />
                     </a>
-                    <a href="https://www.linkedin.com/in/tapesh-patel-276a65a4/" target="_blank" rel="noopener noreferrer">
+                    <a
+                        href="https://www.linkedin.com/in/tapesh-patel-276a65a4/"
+                        target="_blank"
+                        rel="noopener noreferrer">
                         <LinkedIn style={{ fontSize: "2vw ", color: "#FE4747" }} />
                     </a>
                     <a href="https://www.instagram.com/tapesh__patel/" target="_blank" rel="noopener noreferrer">

18a32ef132a528bf63538c46d152830f4301101b
tapesh02 <34514858+tapesh02@users.noreply.github.com>
2024-04-04 23:16:46 +0100

    Fixed resume download issue

--- src/components/Pages/AboutMe/AboutMe.jsx
+++ src/components/Pages/AboutMe/AboutMe.jsx
@@ -18,70 +18,71 @@ import AboutHobbies from "./AboutHobbies";
 import ContactForm from "./ContactForm";
 
 const AboutMe = () => {
-    const onDownloadCv = () => {
-        saveAs("/files/Tapesh's Resume.docx", "Tapesh's Resume.docx");
-    };
+  const onDownloadCv = () => {
+    saveAs("/files/Tapesh's Resume.docx", "Tapesh's Resume.docx");
+  };
 
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const handleToggle = () => {
-        toggleVisible(300, setVisible);
-    };
+  const handleToggle = () => {
+    toggleVisible(300, setVisible);
+  };
 
-    const handleScroll = () => {
-        scrollToTop(0, "smooth");
-    };
+  const handleScroll = () => {
+    scrollToTop(0, "smooth");
+  };
 
+  useEffect(() => {
     window.addEventListener("scroll", handleToggle);
+    return () => {
+      window.removeEventListener("scroll", handleToggle);
+    };
+  }, []);
 
-    useEffect(() => {
-        Aos.init({ duration: 1000 });
-    }, []);
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
-    return (
-        <>
-            <Box className="main-about">
-                <Box className="img-box">
-                    <img src={getImage("profileImg")} alt="profileImg" width="90%" height="90%" />
-                </Box>
-                <Box className="content-box">
-                    <Box className="main-about-title-intro">
-                        <Typography className="about-title">
-                            React Developer, Front-End Developer & UI/UX Designer
-                        </Typography>
-                        <Typography className="about-intro">
-                            I'm obsessed with making things creative and more keen on making things better. I have been
-                            working as a web developer and software developer for the past few years. I wrote my first
-                            HTML code when I was 16 years old, which then made me more curious about pursuing my career
-                            as a web designer and developer during my college days. Moreover, I have been actively
-                            involved in creating the best web designs for my clients, which are mostly start-ups, and
-                            some of them are large business firms. I also started freelancing when I completed my
-                            master's in Ireland, where I helped people who are in need of some creative web design
-                            within a limited time span.
-                        </Typography>
-                        <Typography className="about-intro">
-                            My specialty lies in creating dynamic and catchy front-end web designs and developing web
-                            apps, as well as designing hybrid mobile apps. Some of skills that I use on daily basis are
-                            React, Azure Devops, and Git version and source control. Apart from work, I also prefer to
-                            go on solo trips, attend developer conferences, watch football matches, and hang out with
-                            friends.
-                        </Typography>
-                    </Box>
-                    <AboutMeDetail />
-                </Box>
-            </Box>
-            <Box style={{ width: "100%", margin: "2% auto", textAlign: "center" }}>
-                <Button variant="contained" className="download-btn" onClick={onDownloadCv}>
-                    Download
-                </Button>
-            </Box>
-            <ArrowUp visible={visible} handleScroll={handleScroll} className={"arrowIcon-btn"} />
-            <AboutTechnologies />
-            <AboutResume />
-            <AboutHobbies />
-            <ContactForm />
-        </>
-    );
+  return (
+    <>
+      <Box className="main-about">
+        <Box className="img-box">
+          <img src={getImage("profileImg")} alt="profileImg" width="90%" height="90%" />
+        </Box>
+        <Box className="content-box">
+          <Box className="main-about-title-intro">
+            <Typography className="about-title">React Developer, Front-End Developer & UI/UX Designer</Typography>
+            <Typography className="about-intro">
+              I'm obsessed with making things creative and more keen on making things better. I have been working as a
+              web developer and software developer for the past few years. I wrote my first HTML code when I was 16
+              years old, which then made me more curious about pursuing my career as a web designer and developer during
+              my college days. Moreover, I have been actively involved in creating the best web designs for my clients,
+              which are mostly start-ups, and some of them are large business firms. I also started freelancing when I
+              completed my master's in Ireland, where I helped people who are in need of some creative web design within
+              a limited time span.
+            </Typography>
+            <Typography className="about-intro">
+              My specialty lies in creating dynamic and catchy front-end web designs and developing web apps, as well as
+              designing hybrid mobile apps. Some of skills that I use on daily basis are React, Azure Devops, and Git
+              version and source control. Apart from work, I also prefer to go on solo trips, attend developer
+              conferences, watch football matches, and hang out with friends.
+            </Typography>
+          </Box>
+          <AboutMeDetail />
+        </Box>
+      </Box>
+      <Box style={{ width: "100%", margin: "2% auto", textAlign: "center" }}>
+        <Button variant="contained" className="download-btn" onClick={onDownloadCv}>
+          Download CV
+        </Button>
+      </Box>
+      <ArrowUp visible={visible} handleScroll={handleScroll} className={"arrowIcon-btn"} />
+      <AboutTechnologies />
+      <AboutResume />
+      <AboutHobbies />
+      <ContactForm />
+    </>
+  );
 };
 
 export default AboutMe;
--- src/components/Pages/Projects/Projects.jsx
+++ src/components/Pages/Projects/Projects.jsx
@@ -15,174 +15,170 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDribbble } from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => {
-    const [articles, setArticles] = useState([]);
-    const [shots, setShots] = useState([]);
-    const [visible, setVisible] = useState(false);
+  const [articles, setArticles] = useState([]);
+  const [shots, setShots] = useState([]);
+  const [visible, setVisible] = useState(false);
 
-    useEffect(() => {
-        const getDribbleShots = async () => {
-            const dribbleAccessToken = `${process.env.REACT_APP_DRIBBLE_ACCESS_TOKEN}`;
-            const dribble = `https://api.dribbble.com/v2/user/shots?access_token=${dribbleAccessToken}&page=1&per_page=10`;
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
 
-            const response = await fetch(dribble);
-            try {
-                const responseJson = await response.json();
-                setShots(responseJson);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        getDribbleShots();
-    }, []);
+  const onClickUrl = (url) => {
+    return () => openInNewTab(url);
+  };
 
-    useEffect(() => {
-        const getMediumPosts = async () => {
-            const mediumApi = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@tapesh.patel";
+  const handleToggle = () => {
+    toggleVisible(300, setVisible);
+  };
 
-            const response = await fetch(mediumApi, { headers: { Accept: "application/json" } });
+  const handleScroll = () => {
+    scrollToTop(0, "smooth");
+  };
 
-            try {
-                const responseJson = await response.json();
-                const data = responseJson.items;
-                setArticles(data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        getMediumPosts();
-    }, []);
+  useEffect(() => {
+    const getDribbleShots = async () => {
+      const dribbleAccessToken = `${process.env.REACT_APP_DRIBBLE_ACCESS_TOKEN}`;
+      const dribble = `https://api.dribbble.com/v2/user/shots?access_token=${dribbleAccessToken}&page=1&per_page=10`;
 
-    useEffect(() => {
-        Aos.init({ duration: 1000 });
-    }, []);
-
-    const openInNewTab = (url) => {
-        const newWindow = window.open(url, "_blank", "noopener,noreferrer");
-        if (newWindow) newWindow.opener = null;
+      const response = await fetch(dribble);
+      try {
+        const responseJson = await response.json();
+        setShots(responseJson);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    const onClickUrl = (url) => {
-        return () => openInNewTab(url);
-    };
+    getDribbleShots();
+  }, []);
 
-    const handleToggle = () => {
-        toggleVisible(300, setVisible);
-    };
+  useEffect(() => {
+    const getMediumPosts = async () => {
+      const mediumApi = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@tapesh.patel";
 
-    const handleScroll = () => {
-        scrollToTop(0, "smooth");
+      const response = await fetch(mediumApi, { headers: { Accept: "application/json" } });
+
+      try {
+        const responseJson = await response.json();
+        const data = responseJson.items;
+        setArticles(data);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    getMediumPosts();
+  }, []);
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
+  useEffect(() => {
     window.addEventListener("scroll", handleToggle);
+    return () => {
+      window.removeEventListener("scroll", handleToggle);
+    };
+  }, []);
 
-    const RenderDribbleShots = () =>
-        shots?.map((shot) => {
-            const { id, updated_at, images, title, html_url } = shot;
-            const { normal } = images;
-            return (
-                <Box
-                    className="project-section"
-                    data-aos="fade-right"
-                    data-aos-offset="300"
-                    data-aos-easing="ease-in-sine"
-                    key={id}>
-                    <Box className="main-card">
-                        <Box className="card-img">
-                            <img src={normal} alt="cardimg" className="card-img-inner" />{" "}
-                        </Box>
-                        <Box className="card-title"> {title} </Box>
-                        <Box className="card-subtitle"> {Date(updated_at).toString().slice(0, 15)}</Box>
-                        <Box className="card-footer">
-                            <Typography className="loadshot-text"> Load Shot </Typography>
-                            <FontAwesomeIcon
-                                className="shots-icon"
-                                icon={faDribbble}
-                                onClick={onClickUrl(`${html_url}`)}
-                            />
-                        </Box>
-                    </Box>
-                </Box>
-            );
-        });
-
-    const RenderMediumArticles = () =>
-        articles.map((article) => {
-            const { link, title, description, pubDate } = article;
-            return (
-                <Box className="main-blog-card " key={link} data-aos="fade-up" data-aos-duration="3000">
-                    <Box className="card-blog-content">
-                        <Box className="blog-content-main">
-                            <p className="blog-content-title">{title}</p>
-                            <Markup className="blog-content-overview" content={description}></Markup>
-                        </Box>
-                        <Box className="blog-content-footer">
-                            <Button
-                                variant="contained"
-                                fontSize="small"
-                                className="read-more-btn"
-                                href={link}
-                                target="_blank">
-                                Read More
-                            </Button>
-                            <p className="blog-footer-date">{pubDate.slice(0, 10)}</p>
-                        </Box>
-                    </Box>
-                </Box>
-            );
-        });
-
-    const RenderAccordion = () =>
-        projects.map((project) => {
-            const { id, projectName, projectLink, projectDescription } = project;
-            return (
-                <Accordion key={id}>
-                    <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel1a-content" id="panel1a-header">
-                        <h3>{projectName} </h3>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            {projectDescription}
-                            <Button href={projectLink} target="_blank" rel="noopener noreferrer" color="inherit">
-                                Click to view
-                            </Button>
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            );
-        });
-
-    return (
-        <>
-            <Box className="main-blog ">
-                <Header mainHeaderClass={"main-blog-header"} headerClass={"blog-header"} headerText={"UI/UX"} />
+  const RenderDribbleShots = () =>
+    shots?.map((shot) => {
+      const { id, updated_at, images, title, html_url } = shot;
+      const { normal } = images;
+      return (
+        <Box
+          className="project-section"
+          data-aos="fade-right"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-sine"
+          key={id}>
+          <Box className="main-card">
+            <Box className="card-img">
+              <img src={normal} alt="cardimg" className="card-img-inner" />{" "}
             </Box>
-            <ArrowUp visible={visible} handleScroll={handleScroll} className={"arrow-iconBtn2"} />
-            <Box className="main-project-section">
-                <RenderDribbleShots />
+            <Box className="card-title"> {title} </Box>
+            <Box className="card-subtitle"> {Date(updated_at).toString().slice(0, 15)}</Box>
+            <Box className="card-footer" onClick={onClickUrl(`${html_url}`)}>
+              <Typography className="loadshot-text">
+                View Shot
+                <FontAwesomeIcon className="shots-icon" icon={faDribbble} />
+              </Typography>
             </Box>
+          </Box>
+        </Box>
+      );
+    });
 
-            <Box className="main-blog ">
-                <Header mainHeaderClass={"main-blog-header"} headerClass={"blog-header"} headerText={"Blogs"} />
-                <div className="cards">
-                    <RenderMediumArticles />
-                </div>
-
-                <Box>
-                    <Header
-                        mainHeaderClass={"main-blog-header"}
-                        headerClass={"blog-header"}
-                        headerText={"Upcoming Projects"}
-                    />
-                    <Box style={{ marginBottom: "1%", marginTop: "1%" }}>
-                        <RenderAccordion />
-                    </Box>
-                </Box>
-                <Box className="main-blog-follow-section">
-                    <p className="blog-follow-title"> Want to get latest update of my blogs?</p>
-                    <p className="blog-follow-subtitle"> Follow me</p>
-                </Box>
-                <Box className="blog-follow-icons">{renderIcon(icons)}</Box>
+  const RenderMediumArticles = () =>
+    articles?.map((article) => {
+      const { link, title, description, pubDate } = article;
+      return (
+        <Box className="main-blog-card " key={link} data-aos="fade-up" data-aos-duration="3000">
+          <Box className="card-blog-content">
+            <Box className="blog-content-main">
+              <p className="blog-content-title">{title}</p>
+              <Markup className="blog-content-overview" content={description}></Markup>
             </Box>
-        </>
-    );
+            <Box className="blog-content-footer">
+              <Button variant="contained" fontSize="small" className="read-more-btn" href={link} target="_blank">
+                Read More
+              </Button>
+              <p className="blog-footer-date">{pubDate.slice(0, 10)}</p>
+            </Box>
+          </Box>
+        </Box>
+      );
+    });
+
+  const RenderAccordion = () =>
+    projects?.map((project) => {
+      const { id, projectName, projectLink, projectDescription } = project;
+      return (
+        <Accordion key={id}>
+          <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel1a-content" id="panel1a-header">
+            <h3>{projectName} </h3>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              {projectDescription}
+              <Button href={projectLink} target="_blank" rel="noopener noreferrer" color="inherit">
+                Click to view
+              </Button>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      );
+    });
+
+  return (
+    <>
+      <Box className="main-blog ">
+        <Header mainHeaderClass={"main-blog-header"} headerClass={"blog-header"} headerText={"UI/UX"} />
+      </Box>
+      <ArrowUp visible={visible} handleScroll={handleScroll} className={"arrow-iconBtn2"} />
+      <Box className="main-project-section">
+        <RenderDribbleShots />
+      </Box>
+
+      <Box className="main-blog ">
+        <Header mainHeaderClass={"main-blog-header"} headerClass={"blog-header"} headerText={"Blogs"} />
+        <div className="cards">
+          <RenderMediumArticles />
+        </div>
+
+        <Box>
+          <Header mainHeaderClass={"main-blog-header"} headerClass={"blog-header"} headerText={"Upcoming Projects"} />
+          <Box style={{ marginBottom: "1%", marginTop: "1%" }}>
+            <RenderAccordion />
+          </Box>
+        </Box>
+        <Box className="main-blog-follow-section">
+          <p className="blog-follow-title"> Want to get latest update of my blogs?</p>
+          <p className="blog-follow-subtitle"> Follow me</p>
+        </Box>
+        <Box className="blog-follow-icons">{renderIcon(icons)}</Box>
+      </Box>
+    </>
+  );
 };
 
 export default Projects;
